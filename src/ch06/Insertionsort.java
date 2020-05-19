46c87fe1b412577f263e25758f656858b36c08f1
Febby kurniawan <febbykurniawan073@gmail.com>
2020-05-19 12:35:10 +0700

    Commit pertama

---
+++ src/ch06/Insertionsort.java
@@ -0,0 +1,18 @@
+package ch06;
+
+public class Insertionsort {
+    public static void main(String[] args) {
+        int[]data ={82,12,41,39,19,26,9,48,20,55,8,32,3};
+        for (int outer = data.length -1; outer > 0; outer--){
+            for (int inter = 0; inter < outer; inter++){
+                if (data[inter] > data[inter + 1]){
+                    int temp = data[inter];
+                    data[inter]=data[inter+1];
+                    data [inter+1]=temp;
+                }
+            }
+        }
+        for (int i = 0; i < data.length; i++)
+            System.out.print(data[i]+",");
+    }
+}
---
+++ src/ch06/Selectionsort.java
@@ -0,0 +1,43 @@
+package ch06;
+ import java.util.Scanner;
+public class Selectionsort{
+    public static void main(String args[])
+    {
+        int size, i, j, temp, small, index, count=0;
+        int arr[] = new int[50];
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Array Size : ");
+        size = scan.nextInt();
+        System.out.print("Enter Array Elements : ");
+        for(i=0; i<size; i++)
+        {
+            arr[i] = scan.nextInt();
+        }
+        System.out.print("Sorting Array using Selection Sort Technique..\n");
+        for(i=0; i<(size-1); i++)
+        {
+            small = arr[i];
+            for(j=(i+1); j<size; j++)
+            {
+                if(small>arr[j])
+                {
+                    small = arr[j];
+                    count++;
+                    index = j;
+                }
+            }
+            if(count!=0)
+            {
+                temp = arr[i];
+                arr[i] = small;
+
+            }
+            count=0;
+        }
+        System.out.print("Now the Array after Sorting is :\n");
+        for(i=0; i<size; i++)
+        {
+            System.out.print(arr[i]+ "  ");
+        }
+    }
+}
